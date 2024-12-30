51083b3b4c3e1c4f534bf41eec331dcbb096d738
mahesh-sn <maheshsn705@gmail.com>
2024-12-30 13:26:04 +0530

    My first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen();
+  }
+}
